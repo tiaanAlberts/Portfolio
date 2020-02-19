c980f74845235da7599dc8db134f86d46a5b6d20
tiaan <tiaanmailbox@gmail.com>
2020-02-19 08:32:33 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const   express     = require('express'),
+        app         = express(),
+        nodemailer  = require('nodemailer');
+        bodyParser  = require('body-parser');
+
+var     port    = process.env.PORT || 3000,
+        ip      = process.env.IP;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+// Index
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+// Contact
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/contact', (req, res) => {
+    var mailOpts, stmpTrans;
+    // Setup nodemailer transport
+    stmpTrans = nodemailer.createTransport({
+        service: 'Gmail',
+        auth:{
+            user: 'tiaanmailbox@gmail.com',
+            pass: '7946134679'
+        }
+    });
+    // Mail Options
+    mailOpts = {
+        from: req.body.firstName + req.body.email,
+        to: 'tiaanmailbox@gmail.com',
+        subject: req.body.email + ' sent a message',
+        html:   '<p><strong>Name:</strong> ' + req.body.firstName + " " + req.body.lastName + '</p>\n\n' + 
+                ' <p><strong>Email:</strong> ' + req.body.email + '<p>\n\n' +
+                ' <p><strong>Message:</strong> ' + req.body.message + '</p>' 
+    };
+
+    stmpTrans.sendMail(mailOpts, function(err, res){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('mail sent');
+        }
+        stmpTrans.close();
+    });
+
+    res.redirect('/');
+    
+});
+
+app.listen(port, ip, () => {
+    console.log('Server has started on port: ' + port);
+});
